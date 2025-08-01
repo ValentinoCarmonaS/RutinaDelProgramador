76b100b2d6baad1d497d4522d80ba1149395de1f
Valentino Carmona <vcarmona@fi.uba.ar>
2025-07-31 21:59:30 -0300

    refact: update the package

--- ejercicios/array/facil/LongestCommonPrefix.java
+++ ejercicios/array/facil/LongestCommonPrefix.java
@@ -2,7 +2,6 @@ package array.facil;
 
 public class LongestCommonPrefix {
     /**
-     * [Longest Common Prefix]
      * Write a function to find the longest common prefix string amongst an array of strings.
         If there is no common prefix, return an empty string "".
         
--- ejercicios/hash_table/facil/RomanToInt.java
+++ ejercicios/hash_table/facil/RomanToInt.java
@@ -6,7 +6,6 @@ import java.util.Map;
 public class RomanToInt {
 
     /**
-     *  [Roman to Integer]
      * Roman numerals are represented by seven different symbols: I, V, X, L, C, D and M.
         Symbol       Value
         I             1
