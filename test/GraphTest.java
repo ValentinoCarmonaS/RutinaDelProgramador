1e668bc86172b846d08e3916b5bde6cd907b71b8
Valentino Carmona <vcarmona@fi.uba.ar>
2025-08-07 00:29:43 -0300

    test(graph) : dia 7 - KeysAndRooms

--- test/GraphTest.java
+++ test/GraphTest.java
@@ -6,6 +6,7 @@ import java.util.Arrays;
 
 import org.junit.Test;
 
+import graph.medio.KeysAndRooms;
 import graph.medio.MaxAreaOfIsland;
 
 public class GraphTest {
@@ -28,5 +29,26 @@ public class GraphTest {
 
         assertEquals(6, mai.maxAreaOfIsland(grid1));
         assertEquals(0, mai.maxAreaOfIsland(grid2));
+    }
+
+    @Test
+    public void testCanVisitAllRooms() {
+        KeysAndRooms kar = new KeysAndRooms();
+        
+        assertEquals(true, kar.canVisitAllRooms(
+                Arrays.asList(
+                    Arrays.asList(1),
+                    Arrays.asList(2),
+                    Arrays.asList(3),
+                    Arrays.asList()
+                )));
+        
+        assertEquals(false, kar.canVisitAllRooms(
+                Arrays.asList(
+                    Arrays.asList(1,3),
+                    Arrays.asList(3,0,1),
+                    Arrays.asList(2),
+                    Arrays.asList(0)
+                )));
     }
 }
