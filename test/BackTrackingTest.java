a8c9c5ca4cf6462b518783f87d3bf9b7734acb88
Valentino Carmona <vcarmona@fi.uba.ar>
2025-08-09 02:11:06 -0300

    test(backTracking): dia 9 - Permute

--- test/BackTrackingTest.java
+++ test/BackTrackingTest.java
@@ -23,4 +23,29 @@ public class BackTrackingTest {
         assertEquals(ans2, lc.letterCombinations(""));
         assertEquals(ans3, lc.letterCombinations("2"));
     }
+
+    @Test
+    public void testPermute() {
+        Permute p = new Permute();
+
+        List<List<Integer>> ans1 = Arrays.asList(
+            Arrays.asList(1,2,3),
+            Arrays.asList(1,3,2),
+            Arrays.asList(2,1,3),
+            Arrays.asList(2,3,1),
+            Arrays.asList(3,1,2),
+            Arrays.asList(3,2,1)
+        );
+        List<List<Integer>> ans2 = Arrays.asList(
+            Arrays.asList(0,1),
+            Arrays.asList(1,0)
+        );
+        List<List<Integer>> ans3 = Arrays.asList(
+            Arrays.asList(1)
+        );
+        
+        assertEquals(ans1, p.permute(new int[] {1,2,3}));
+        assertEquals(ans2, p.permute(new int[] {0, 1}));
+        assertEquals(ans3, p.permute(new int[] {1}));
+    }
 }
