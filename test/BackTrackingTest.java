de12c986e4478f4f2c9d8abe2031a9986dd4e8fa
Valentino Carmona <vcarmona@fi.uba.ar>
2025-08-12 20:20:14 -0300

    test(BackTracking): dia 13 - Subsets

--- test/BackTrackingTest.java
+++ test/BackTrackingTest.java
@@ -48,4 +48,28 @@ public class BackTrackingTest {
         assertEquals(ans2, p.permute(new int[] {0, 1}));
         assertEquals(ans3, p.permute(new int[] {1}));
     }
+
+    @Test
+    public void testSubsets() {
+        Subsets s = new Subsets();
+
+        List<List<Integer>> ans1 = Arrays.asList(
+            Arrays.asList(),
+            Arrays.asList(1),
+            Arrays.asList(1,2),
+            Arrays.asList(1,2,3),
+            Arrays.asList(1,3),
+            Arrays.asList(2),
+            Arrays.asList(2,3),
+            Arrays.asList(3)
+        );
+        
+        List<List<Integer>> ans2 = Arrays.asList(
+            Arrays.asList(),
+            Arrays.asList(0)
+        );
+
+        assertEquals(ans1, s.subsets(new int[] {1,2,3}));
+        assertEquals(ans2, s.subsets(new int[] {0}));
+    }
 }
