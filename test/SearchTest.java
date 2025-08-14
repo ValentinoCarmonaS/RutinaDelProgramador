976a2638fb5e5faaa66e0c51115064ba33073f29
Valentino Carmona <vcarmona@fi.uba.ar>
2025-08-14 13:16:34 -0300

    test(search): dia 14 - SearchRange and Search

--- test/SearchTest.java
+++ test/SearchTest.java
@@ -1,27 +1,29 @@
 package test;
 
+import static org.junit.Assert.assertArrayEquals;
 import static org.junit.Assert.assertEquals;
 
 import org.junit.Test;
 
 import helpers.TreeNode;
 import search.facil.*;
+import search.medio.*;
 
 public class SearchTest {
-    
+
     @Test
     public void testSearchInsertPosition() {
         SearchInsertPosition sip = new SearchInsertPosition();
 
-        assertEquals(2, sip.searchInsert(new int[] {1,3,5,6}, 5));
-        assertEquals(1, sip.searchInsert(new int[] {1,3,5,6}, 2));
-        assertEquals(4, sip.searchInsert(new int[] {1,3,5,6}, 7));
+        assertEquals(2, sip.searchInsert(new int[] { 1, 3, 5, 6 }, 5));
+        assertEquals(1, sip.searchInsert(new int[] { 1, 3, 5, 6 }, 2));
+        assertEquals(4, sip.searchInsert(new int[] { 1, 3, 5, 6 }, 7));
     }
 
     @Test
     public void testCountTreeNodes() {
         CountTreeNodes ctn = new CountTreeNodes();
-        
+
         TreeNode ab4 = new TreeNode(4);
         TreeNode ab5 = new TreeNode(5);
         TreeNode ab6 = new TreeNode(6);
@@ -32,5 +34,22 @@ public class SearchTest {
         TreeNode ab1 = new TreeNode(1, ab2, ab3);
 
         assertEquals(6, ctn.countNodes(ab1));
+    }
+
+    @Test
+    public void testSearchRange() {
+        SearchRange sr = new SearchRange();
+
+        assertArrayEquals(new int[] { 3, 4 }, sr.searchRange(new int[] { 5, 7, 7, 8, 8, 10 }, 8));
+        assertArrayEquals(new int[] { -1, -1 }, sr.searchRange(new int[] { 5, 7, 7, 8, 8, 10 }, 6));
+        assertArrayEquals(new int[] { -1, -1 }, sr.searchRange(new int[] {}, 0));
+    }
+
+    @Test
+    public void testSearch() {
+        Search s = new Search();
+
+        assertEquals(4, s.search(new int[] { -1,0,3,5,9,12 }, 9));
+        assertEquals(-1, s.search(new int[] { -1,0,3,5,9,12 }, 2));
     }
 }
