dc1a8e83ed1d725fbc98f98f07405d865a5d4972
Valentino Carmona <vcarmona@fi.uba.ar>
2026-02-08 19:47:40 -0300

    feat(Math): Divisible and Non-divisible Sums Difference

--- test/MathTest.java
+++ test/MathTest.java
@@ -4,6 +4,7 @@ import static org.junit.Assert.assertEquals;
 
 import org.junit.Test;
 
+import math.facil.DifferenceOfSums;
 import math.facil.TheMaximumAchievableX;
 
 public class MathTest {
@@ -12,5 +13,12 @@ public class MathTest {
     public void testTheMaximumAchievableX() {
         assertEquals(6, TheMaximumAchievableX.theMaximumAchievableX(4, 1));
         assertEquals(7, TheMaximumAchievableX.theMaximumAchievableX(3, 2));
+    }
+
+    @Test
+    public void testDifferenceOfSums() {
+        assertEquals(19, DifferenceOfSums.differenceOfSums(10, 3));
+        assertEquals(15, DifferenceOfSums.differenceOfSums(5, 6));
+        assertEquals(-15, DifferenceOfSums.differenceOfSums(5, 1));
     }
 }
